caef2483e3cd058329ebb1b736f44775620aaee5
s1f102100704 <s1f102100704@iniad.org>
2024-04-24 04:37:01 +0900

    上方向ならひっくり返せる

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
-import { useSearchParams } from 'next/navigation';
+// import { useSearchParams } from 'next/navigation';
 import styles from './index.module.css';
 import { useState } from 'react';
-import { reportWebVitals } from 'next/dist/build/templates/pages';
+// import { reportWebVitals } from 'next/dist/build/templates/pages';
 
 const Home = () => {
   const [turnColor, setTurncolor] = useState(1);
   const [board, setBoard] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [2, 0, 0, 0, 0, 0, 0, 2],
+    [2, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 1, 2, 0, 0, 2],
+    [0, 0, 0, 2, 1, 0, 0, 1],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -19,21 +19,29 @@ const Home = () => {
     const newBoard = structuredClone(board);
     newBoard[y][x] = turnColor;
     setTurncolor(turnColor === 1 ? 2 : 1);
-    if (newBoard[y - 1][x] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
+
+    // ひっくり返す処理
+
+    for (let n = 2, i = 1, z = 1000; y - n >= 0 && z > 100; n++, i++) {
+      if (newBoard[y - n][x] === turnColor) {
+        console.log('a');
+        for (let p = 1; p < n; p++) {
+          console.log('b:', n);
+          newBoard[y - n + p][x] = turnColor;
+          //すべてひっくり返さないようにする
+          if (newBoard[y - n + i][x] === turnColor) {
+            console.log('break');
+            z = 0;
+            break;
+          }
+        }
+      }
     }
-    if (newBoard[y + 1][x] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
-    }
-    if (newBoard[y][x - 1] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
-    }
-    if (newBoard[y][x + 1] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
-    }
+
     setBoard(newBoard);
-    console.log(x, y);
+    // console.log(x, y);
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.boardstyle}>
